e16c72450e0e73eb07f1fa1c63bee876236d43c0
ulyanovikovak <yasenevsk.18@gmail.com>
2025-04-21 14:28:41 +0300

    fix spending_id

--- backend/courtcosts/user/urls.py
+++ backend/courtcosts/user/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import RegisterView, LoginView, LogoutView, CalculationCreateView, SpendingCalculationCreateView, \
-    ProfileView, UserUpdateView, CalculationDetailView
+    ProfileView, UserUpdateView, CalculationDetailView, SpendingCalculationDetailView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
@@ -29,6 +29,8 @@ urlpatterns = [
 
     # Эндпоинт для создания SpendingCalculation внутри Calculation (только для авторизованных пользователей)
     path('spending/create/', SpendingCalculationCreateView.as_view(), name='spending-calculation-create'),
+
+    path('spendings/<int:spending_id>/', SpendingCalculationDetailView.as_view(), name='spending-detail'),
 
     path('calculation/<int:calculation_id>/details/', CalculationDetailView.as_view(), name='calculation-detail'),
 
--- backend/courtcosts/user/views.py
+++ backend/courtcosts/user/views.py
@@ -182,3 +182,28 @@ class CalculationDetailView(APIView):
             'calculation': calc_data,
             'spendings': spendings_data
         }, status=status.HTTP_200_OK)
+
+class SpendingCalculationDetailView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, user, spending_id):
+        return get_object_or_404(SpendingCalculation, id=spending_id, calculation__user=user)
+
+    def get(self, request, spending_id):
+        spending = self.get_object(request.user, spending_id)
+        serializer = SpendingCalculationSerializer(spending)
+        return Response(serializer.data)
+
+    def put(self, request, spending_id):
+        spending = self.get_object(request.user, spending_id)
+        serializer = SpendingCalculationSerializer(spending, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, spending_id):
+        spending = self.get_object(request.user, spending_id)
+        spending.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
